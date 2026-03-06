5fcca6a5304732d1fd2bf5c5c78b4d081eaf9b9f
diegohenao819 <diegohenao819@gmail.com>
2026-03-06 11:17:01 -0500

    cambios en el redirect al proxy

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,3 @@
-import { redirect } from "next/navigation";
-import { createClient } from "@/lib/supabase/server";
-import { Suspense } from "react";
-
-async function RootRedirect() {
-  const supabase = await createClient();
-  const { data } = await supabase.auth.getUser();
-
-  if (data.user) {
-    redirect("/inicio");
-  } else {
-    redirect("/auth/login");
-  }
-   return null; 
-}
-
 export default function RootPage() {
-  return (
-    <Suspense>
-      <RootRedirect />
-    </Suspense>
-  );
+  return null;
 }
